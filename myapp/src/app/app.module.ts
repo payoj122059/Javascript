bd93b0c9703da05b5319a166dd82452886888b29
Payoj Prasad <payoj1994@gmail.com>
2019-03-15 10:24:28 +0530

    modal pop-up creation

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Log-in/login/login.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbdModalBasic} from './Log-in/login/modal-basic';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NgbdModalBasic
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
